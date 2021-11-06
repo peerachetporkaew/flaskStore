96b04edca7181b02f2e9beca59226a6cb1ca9977
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2021-11-06 20:24:07 +0700

    update port

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def index():
     return "HELLO WORLD"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=int(os.environ.get('PORT',33507)))
+    app.run(host="0.0.0.0",port=int(os.environ.get('PORT',5000)))
